774e460c1b5283f81082354d5fbe62d16f0c95d4
punit-iitd <58264601+punit-iitd@users.noreply.github.com>
2020-07-23 20:05:50 +0530

    Add files via upload

--- ReNet_new/src/Circ_list_Node.java
+++ ReNet_new/src/Circ_list_Node.java
@@ -1,5 +1,5 @@
 public class Circ_list_Node<T> {
-	Circ_list_Node<T> next;
-	Circ_list_Node<T> prev;
-	T val;
+	Circ_list_Node<T> next;                //pointer of the next node
+	Circ_list_Node<T> prev;                //pointer of the previous node(or parent)
+	T val;                                 //denotes the value of the node (key)
 }

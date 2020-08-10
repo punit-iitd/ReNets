c02de8cbc08be0f8124325041655c60e26609c58
punit-iitd <58264601+punit-iitd@users.noreply.github.com>
2020-08-10 12:11:15 +0530

    Add files via upload

---
+++ ReNet_new/src/BTreePrinter.java
@@ -0,0 +1,87 @@
+import java.util.*;
+public class BTreePrinter<T extends Comparable<T>> {
+
+    public static <T extends Comparable<T>> void printSplay_node(Splay_node<T> root) {
+        int maxLevel = BTreePrinter.maxLevel(root);
+
+        printSplay_nodeInternal(Collections.singletonList(root), 1, maxLevel);
+    }
+
+    private static <T extends Comparable<T>> void printSplay_nodeInternal(List<Splay_node<T>> nodes, int level, int maxLevel) {
+        if (nodes.isEmpty() || BTreePrinter.isAllElementsNull(nodes))
+            return;
+
+        int floor = maxLevel - level;
+        int endgeLines = (int) Math.pow(2, (Math.max(floor - 1, 0)));
+        int firstSpaces = (int) Math.pow(2, (floor)) - 1;
+        int betweenSpaces = (int) Math.pow(2, (floor + 1)) - 1;
+
+        BTreePrinter.printWhitespaces(firstSpaces);
+
+        List<Splay_node<T>> newSplay_nodes = new ArrayList<Splay_node<T>>();
+        for (Splay_node<T> node : nodes) {
+            if (node != null) {
+                System.out.print(node.key);
+                newSplay_nodes.add(node.left);
+                newSplay_nodes.add(node.right);
+            } else {
+                newSplay_nodes.add(null);
+                newSplay_nodes.add(null);
+                System.out.print(" ");
+            }
+
+            BTreePrinter.printWhitespaces(betweenSpaces);
+        }
+        System.out.println("");
+
+        for (int i = 1; i <= endgeLines; i++) {
+            for (int j = 0; j < nodes.size(); j++) {
+                BTreePrinter.printWhitespaces(firstSpaces - i);
+                if (nodes.get(j) == null) {
+                    BTreePrinter.printWhitespaces(endgeLines + endgeLines + i + 1);
+                    continue;
+                }
+
+                if (nodes.get(j).left != null)
+                    System.out.print("/");
+                else
+                    BTreePrinter.printWhitespaces(1);
+
+                BTreePrinter.printWhitespaces(i + i - 1);
+
+                if (nodes.get(j).right != null)
+                    System.out.print("\\");
+                else
+                    BTreePrinter.printWhitespaces(1);
+
+                BTreePrinter.printWhitespaces(endgeLines + endgeLines - i);
+            }
+
+            System.out.println("");
+        }
+
+        printSplay_nodeInternal(newSplay_nodes, level + 1, maxLevel);
+    }
+
+    private static void printWhitespaces(int count) {
+        for (int i = 0; i < count; i++)
+            System.out.print(" ");
+    }
+
+    private static <T extends Comparable<T>> int maxLevel(Splay_node<T> node) {
+        if (node == null)
+            return 0;
+
+        return Math.max(BTreePrinter.maxLevel(node.left), BTreePrinter.maxLevel(node.right)) + 1;
+    }
+
+    private static <T> boolean isAllElementsNull(List<T> list) {
+        for (Object object : list) {
+            if (object != null)
+                return false;
+        }
+
+        return true;
+    }
+
+}
